f951b1cc427d56e276b1e22117ff25ac4b6a9e83
jul62144-cmyk <jul62144@gmail.com>
2026-09-03 11:30:27 +0200

    Bump cache for autonomous RLT import

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "trajets-hdf-v2.14.18-vercel";
+const CACHE = "trajets-hdf-v2.14.19-vercel";
 const ASSETS = [
   "/", "/index.html", "/style.css", "/v21416-sharp-rlt.css", "/app.js",
   "/v2133-patch.js", "/v2134-dedupe.js", "/v2135-asct-viewer.js",
